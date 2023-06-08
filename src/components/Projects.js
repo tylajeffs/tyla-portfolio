b8f1d778f79368a97b4fd5330a84d3a6e49b60e6
Tyla Jeffs <tylajeffs@gmail.com>
2023-06-08 15:58:07 +0800

    fix layout, add hover effects for different projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,8 +9,48 @@ function Projects() {
                     all
                 </div>
                 <div className='column'>
+
+                <div className='project-div short-project green-column'>
+                        <div className='overlay'>
+                            <h2 className='project-name'>Project 2</h2>
+                            <p className='project-description'>description</p>
+                            <div className='tag-row'>
+                                <div className='tag'>Javascript</div>
+                                <div className='tag'>CSS</div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className='project-div tall-project project-3'>
+                        <div className='overlay'>
+                            <h2 className='project-name'>Project 3</h2>
+                            <p className='project-description'>description</p>
+                            <div className='tag-row'>
+                                <div className='tag'>Javascript</div>
+                                <div className='tag'>CSS</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='project-div short-project project-5'>
+                        <div className='overlay'>
+                            <h2 className='project-name'>Project 5</h2>
+                            <p className='project-description'>despcription</p>
+                            <div className='tag-row'>
+                                <div className='tag'>Javascript</div>
+                                <div className='tag'>CSS</div>
+                            </div>
+                        </div>
+                    </div>
+
                     
-                    <div className='project-div minesweeper'>
+
+                </div>
+
+                <div className='column'>
+
+                    <div className='project-div tall-project minesweeper'>
                         <div className='overlay'>
                             <h2 className='project-name'>Minesweeper</h2>
                             <p className='project-description'>recreation of the classic minesweeper</p>
@@ -18,18 +58,32 @@ function Projects() {
                                 <div className='tag'>Javascript</div>
                                 <div className='tag'>CSS</div>
                             </div>
-
                         </div>
-                        
-
                     </div>
-                </div>
 
-                <div className='column'>
-                    <div className='project-div green-column'>
-                        here's some text
-
+                    <div className='project-div tall-project project-4'>
+                        <div className='overlay'>
+                            <h2 className='project-name'>Project 4</h2>
+                            <p className='project-description'>despcription</p>
+                            <div className='tag-row'>
+                                <div className='tag'>Javascript</div>
+                                <div className='tag'>CSS</div>
+                            </div>
+                        </div>
                     </div>
+
+                    <div className='project-div short-project project-6'>
+                        <div className='overlay'>
+                            <h2 className='project-name'>Project 6</h2>
+                            <p className='project-description'>despcription</p>
+                            <div className='tag-row'>
+                                <div className='tag'>Javascript</div>
+                                <div className='tag'>CSS</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    
 
                 </div>
 
