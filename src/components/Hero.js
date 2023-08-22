5025b77fd8049288af64c9dd8e995c222ffc7e28
Tyla Jeffs <tylajeffs@gmail.com>
2023-08-22 14:02:26 -0500

    Add clouds onto light mode hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import "../css/hero.css"
+import cloudll from "../assets/light-mode-cloud-left.png"
+import cloudlr from "../assets/light-mode-cloud-right.png"
+import clouddl from "../assets/dark-mode-cloud-left.png"
+import clouddr from "../assets/dark-mode-cloud-right.png"
+
 
 
 function Hero() {
     return (
-        <div className="hero-background">
+        <div className="hero">
 
             <h1 className="hero-text">Hey! I'm Tyla</h1>
+            
+            <img src={cloudll}/>
+            <img src={cloudlr}/>
+
+           
 
         </div>
     )
