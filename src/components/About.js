b0a56426b580f06836ffbc10fc03d9eec47efb8e
Tyla Jeffs <tylajeffs@gmail.com>
2023-09-04 12:37:33 -1000

    add slider functionality to change bio text on change

--- src/components/About.js
+++ src/components/About.js
@@ -1,101 +1,100 @@
 import React from "react";
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
+import { useState } from 'react';
 import "../css/about.css";
 
 
 function About() {
 
+  //state variable to keep track of the slider value
+  const [sliderValue, setSliderValue] = useState(50);
 
-    //array of all the options/labels for the bio length slider
-    const options = [
-        {
-          value: 0,
-          label: 'Shortest',
-        },
-        {
-          value: 25,
-          label: 'Short',
-        },
-        {
-          value: 50,
-          label: 'Normal',
-        },
-        {
-          value: 75,
-          label: 'Long',
-        },
-        {
-          value: 100,
-          label: 'Longest',
-        },
-    ];
+  //function to change the slider value and render the bio
+  const changeSliderValue = (event, sliderValue) => {
+    setSliderValue(sliderValue);
+    renderBio(sliderValue);
+  };
 
 
+  //array of all the options/labels for the bio length slider
+  const options = [
+      {
+        value: 0,
+        label: 'Shortest',
+      },
+      {
+        value: 25,
+        label: 'Short',
+      },
+      {
+        value: 50,
+        label: 'Normal',
+      },
+      {
+        value: 75,
+        label: 'Long',
+      },
+      {
+        value: 100,
+        label: 'Longest',
+      },
+  ];
 
-    //function to create the slider
-    function createSlider() {
-        return (
-          <Box sx={{ width: 600 }}>
-            <Slider
-              aria-label="Bio Length"
-              defaultValue={50}
-              step={25}
-              valueLabelDisplay="off"
-              marks={options}
-            />
-          </Box>
-        );
+
+  //function to create the slider
+  function createSlider() {
+      return (
+        <Box sx={{ width: 600 }}>
+          <Slider
+            aria-label="Bio Length"
+            value={sliderValue}
+            step={25}
+            valueLabelDisplay="off"
+            marks={options}
+            onChange={changeSliderValue}
+          />
+        </Box>
+      );
+    }
+
+
+  //function to decide which length of bio to render
+  function selectBioLength(param) {
+      switch(param) {
+          case 0:
+              return "shortest bio";
+          case 25:
+              return "short bio";
+          case 50:
+              return "normal bio";
+          case 75:
+              return "long bio";
+          case 100:
+              return "longest bio";
       }
+  }
 
 
+  //function to actually render the bio 
+  function renderBio(param) {
+      return(
+          <div>
+              {selectBioLength(param)}
+          </div>
+      )
+  }
 
-
-    //function to decide which length of bio to render
-    function renderBio(param) {
-        switch(param) {
-            case 'shortest':
-                return "shortest bio";
-            case 'short':
-                return "short bio";
-            case 'normal':
-                return "normal bio";
-            case 'long':
-                return "long bio";
-            case 'longest':
-                return "longest bio";
-        }
-    }
-
-    //function to actually render the bio 
-    function render() {
-        return(
-            <div>
-                {renderBio('short')}
-            </div>
-        )
-    }
 
 
   return (
     <div className="about-section">
 
-
       {createSlider()}
 
-      <div className="bio">
-          {renderBio('longest')}
-      </div>
-
-      <div>
-          {render()}
-      </div>
-
+      {renderBio(sliderValue)}
 
     </div>
-
-
-
   );
 };
 
